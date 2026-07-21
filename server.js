49e467818e361d1e3ef3e61374f1c267a57a7377
Abhijith S Sanal <abhijithssanal10@gmail.com>
2026-07-21 20:03:46 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('MediConnect Backend is Running!')
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
