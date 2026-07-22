84ce0fbd1cf881cebd7c51c38cb122ded9938343
Abhijith S Sanal <abhijithssanal10@gmail.com>
2026-07-22 16:55:36 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
+const mongoose = require('mongoose')
 
 dotenv.config()
 
@@ -8,6 +9,11 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB Connected Successfully!'))
+  .catch((err) => console.log('MongoDB Connection Error:', err))
 
 // Test route
 app.get('/', (req, res) => {
